b2a51ba4eeae3ef8d6a2e2fa5d9af557e8419540
Eugene <0x1x3x@protonmail.com>
2023-02-01 17:21:08 +0100

    carousel-bootstrap

---
+++ image-slider/src/App.js
@@ -0,0 +1,12 @@
+import CarouselBox from "./components/CarouselBox";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <CarouselBox />
+    </>
+  );
+}
+
+export default App;

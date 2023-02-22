92cd5258ad0459abc2f94b6d9e72e0e3117e21b2
Eugene <0x1x3x@protonmail.com>
2023-02-22 10:27:21 +0100

    .

---
+++ glass-player/src/components/Player/tracks.jsx
@@ -0,0 +1,44 @@
+exports.tracksData = [
+  {
+    title: "Koche Podcast | 002",
+    artist: "Dreimal T (Vinyl Only)",
+    url: "/tracks/002.mp3",
+    image: "/img/002.jpg", // указать адрес
+  },
+  {
+    title: "Koche Podcast | 014",
+    artist: "Matthias Lindner (Vinyl Only)",
+    url: "/tracks/014.mp3",
+    image: "/img/014.jpg", // указать адрес
+  },
+  {
+    title: "Koche Podcast | 016",
+    artist: "Moriòn (Vinyl Only)",
+    url: "/tracks/016.mp3",
+    image: "/img/016.jpg", // указать адрес
+  },
+  {
+    title: "Koche Podcast | 018",
+    artist: "Phil dB+ (Vinyl Only)",
+    url: "/tracks/018.mp3",
+    image: "/img/018.jpg", // указать адрес
+  },
+  {
+    title: "Koche Podcast | 022",
+    artist: "Hari (Vinyl Only)",
+    url: "/tracks/022.mp3",
+    image: "/img/022.jpg", // указать адрес
+  },
+  {
+    title: "Koche Podcast | 023",
+    artist: "Filin (Vinyl Only)",
+    url: "/tracks/023.mp3",
+    image: "/img/023.jpg", // указать адрес
+  },
+  {
+    title: "Koche Podcast | 025",
+    artist: "Microxika (Vinyl Only)",
+    url: "/tracks/025.mp3",
+    image: "/img/025.jpg", // указать адрес
+  },
+];
